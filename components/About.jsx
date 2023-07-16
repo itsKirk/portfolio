b09115942b44a3af9ac170c151d57bd4154b5572
Kirk <kirkrepository@gmail.com>
2023-07-16 14:12:11 +0300

    coffee link

--- components/About.jsx
+++ components/About.jsx
@@ -1,29 +1,39 @@
-import Image from 'next/image'
-import React from 'react'
-
 function About() {
-    return (
-        <div name='about' className="h-screen text-white w-full bg-[#0a192f]">
-            <div className='flex justify-center items-center w-full h-full flex-col'>
-                <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-                    <div className=' sm:text-right pb-8 pl-4 '>
-                        <p className='text-4xl font-bold inline border-b-4 border-[#7abe41] '>About</p>
-                    </div>
-                    <div className='hidden md:text-right md:inline'>
-                        {/* <Image src={'/hero.png'} height={100} width={100} alt='hero' /> */}
-                    </div>
-                </div>
-                <div className='max-w-[1000px] w-full py-4 px-4 grid sm:grid-cols-2 gap-8'>
-                    <div className='sm:text-right pb-4 pl-4 text-3xl font-bold'>
-                        <p>Hi, I&apos;m Patrick. It&apos;s nice to have you here and I&apos;d like you to check around my website.</p>
-                    </div>
-                    <div className='text-xs pl-4 pb-4 sm:text-lg md:text-xl'>
-                        <p >I am a web developer and I am very much passionate about building full stack applications. I specialize in creating software for clients ranging from individuals and small businesses to large companies. What would you do if you had a software expert available at your fingertips?</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      name="about"
+      className="h-screen text-white w-full bg-[#0a192f]">
+      <div className="flex justify-center items-center w-full h-full flex-col">
+        <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
+          <div className=" sm:text-right pb-8 pl-4 ">
+            <p className="text-4xl font-bold inline border-b-4 border-[#7abe41] ">
+              About
+            </p>
+          </div>
+          <div className="hidden md:text-right md:inline">
+            {/* <Image src={'/hero.png'} height={100} width={100} alt='hero' /> */}
+          </div>
         </div>
-    )
+        <div className="max-w-[1000px] w-full py-4 px-4 grid sm:grid-cols-2 gap-8">
+          <div className="sm:text-right pb-4 pl-4 text-3xl font-bold">
+            <p>
+              Hi, I&apos;m Patrick. It&apos;s nice to have you here and I&apos;d
+              like you to check around my website.
+            </p>
+          </div>
+          <div className="text-xs pl-4 pb-4 sm:text-lg md:text-xl">
+            <p>
+              Welcome to my website! I&apos;m a web developer with expertise in
+              full-stack application development. From individuals to large
+              companies, I create custom software solutions. Let&apos;s
+              collaborate for exceptional web development. Explore my portfolio
+              to see my expertise in action.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,73 +1,188 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { FaBars, FaTimes, FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsPersonLinesFill } from 'react-icons/bs'
-import Link from 'next/link'
-import logo from '../public/logo.png'
-import { Link as Switch } from 'react-scroll'
+import Image from "next/image";
+import { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedinIn,
+  FaTwitter,
+  FaCoffee,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsPersonLinesFill } from "react-icons/bs";
+import Link from "next/link";
+import logo from "../public/logo.png";
+import { Link as Switch } from "react-scroll";
 function Navbar({ children }) {
-    const [mobileMenu, setMobileMenu] = useState(false)
-    const handleMobileMenu = () => setMobileMenu(!mobileMenu)
-    return (
-        <>
-            <div className='fixed w-full mx-auto h-[80px] flex z-[55]  justify-between items-center px-24 bg-[#0a192f] text-gray-300 mb-[50px]'>
-                {/* logo/brand */}
-                <Switch to='home' smooth={true}>
-                    <Image src={logo} height={200} width={200} alt='hero' className='left-0 cursor-pointer' />
-                </Switch>
-                {/* menu */}
-                <ul className='hidden md:flex absolute right-28'>
-                    <Switch to='home' smooth={true} className='text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]'>Home</Switch>
-                    <Switch to='about' smooth={true} className='text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]'>About</Switch>
-                    <Switch to='skills' smooth={true} className='text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]'>Skills</Switch>
-                    <Switch to='work' smooth={true} className='text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]'>Work</Switch>
-                    <Switch to='contact' smooth={true} className='text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]'>Contact</Switch>
-                </ul>
-                {/* hamburger */}
-                <div onClick={handleMobileMenu} className="z-10 md:hidden cursor-pointer">
-                    {mobileMenu ? <FaTimes /> : <FaBars />}
-                </div>
-                {/* mobile menu */}
-                <ul className={mobileMenu ? 'absolute top-0 left-0 bg-[#0a192f] w-full h-screen flex flex-col justify-center items-center md:hidden ' : 'hidden'}>
-                    <Switch to='home' smooth={true} onClick={handleMobileMenu} className='py-6 text-4xl font-bold  cursor-pointer hover:text-[#7abe41]'>Home</Switch>
-                    <Switch to='about' smooth={true} onClick={handleMobileMenu} className='py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]'>About</Switch>
-                    <Switch to='skills' smooth={true} onClick={handleMobileMenu} className='py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]'>Skills</Switch>
-                    <Switch to='work' smooth={true} onClick={handleMobileMenu} className='py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]'>Work</Switch>
-                    <Switch to='contact' smooth={true} onClick={handleMobileMenu} className='py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]'>Contact</Switch>
-                </ul>
-            </div>
-            {/* social icons */}
-            <div className='hidden md:flex fixed flex-col justify-evenly left-0 top-[35%]'>
-                <ul>
-                    <Link href='https://www.Linkedin.com/in/drwambua/' target='_blank' className='flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer'>
-                        <span className='font-bold text-white'>LinkedIn</span>
-                        <FaLinkedinIn className='text-[#7abe41]' size={30} />
-                    </Link>
-                    <Link href='https://twitter.com/DrWambua' target='_blank' className='flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer'>
-                        <span className='font-bold text-white'>Twitter</span>
-                        <FaTwitter size={30} className='text-[#7abe41]' />
-                    </Link>
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const handleMobileMenu = () => setMobileMenu(!mobileMenu);
+  return (
+    <>
+      <div className="fixed w-full mx-auto h-[80px] flex z-[55]  justify-between items-center px-24 bg-[#0a192f] text-gray-300 mb-[50px]">
+        {/* logo/brand */}
+        <Switch
+          to="home"
+          smooth={true}>
+          <Image
+            src={logo}
+            height={200}
+            width={200}
+            alt="hero"
+            className="left-0 cursor-pointer"
+          />
+        </Switch>
+        {/* menu */}
+        <ul className="hidden md:flex absolute right-28">
+          <Switch
+            to="home"
+            smooth={true}
+            className="text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]">
+            Home
+          </Switch>
+          <Switch
+            to="about"
+            smooth={true}
+            className="text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]">
+            About
+          </Switch>
+          <Switch
+            to="skills"
+            smooth={true}
+            className="text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]">
+            Skills
+          </Switch>
+          <Switch
+            to="work"
+            smooth={true}
+            className="text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]">
+            Work
+          </Switch>
+          <Switch
+            to="contact"
+            smooth={true}
+            className="text-lg cursor-pointer font-normal space-x-4 mx-2 hover:text-[#7abe41]">
+            Contact
+          </Switch>
+        </ul>
+        {/* hamburger */}
+        <div
+          onClick={handleMobileMenu}
+          className="z-10 md:hidden cursor-pointer">
+          {mobileMenu ? <FaTimes /> : <FaBars />}
+        </div>
+        {/* mobile menu */}
+        <ul
+          className={
+            mobileMenu
+              ? "absolute top-0 left-0 bg-[#0a192f] w-full h-screen flex flex-col justify-center items-center md:hidden "
+              : "hidden"
+          }>
+          <Switch
+            to="home"
+            smooth={true}
+            onClick={handleMobileMenu}
+            className="py-6 text-4xl font-bold  cursor-pointer hover:text-[#7abe41]">
+            Home
+          </Switch>
+          <Switch
+            to="about"
+            smooth={true}
+            onClick={handleMobileMenu}
+            className="py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]">
+            About
+          </Switch>
+          <Switch
+            to="skills"
+            smooth={true}
+            onClick={handleMobileMenu}
+            className="py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]">
+            Skills
+          </Switch>
+          <Switch
+            to="work"
+            smooth={true}
+            onClick={handleMobileMenu}
+            className="py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]">
+            Work
+          </Switch>
+          <Switch
+            to="contact"
+            smooth={true}
+            onClick={handleMobileMenu}
+            className="py-6 text-4xl font-bold cursor-pointer hover:text-[#7abe41]">
+            Contact
+          </Switch>
+        </ul>
+      </div>
+      {/* social icons */}
+      <div className="hidden md:flex fixed flex-col justify-evenly left-0 top-[35%]">
+        <ul>
+          <Link
+            href="https://www.Linkedin.com/in/drwambua/"
+            target="_blank"
+            className="flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white">LinkedIn</span>
+            <FaLinkedinIn
+              className="text-[#7abe41]"
+              size={30}
+            />
+          </Link>
+          <Link
+            href="https://twitter.com/DrWambua"
+            target="_blank"
+            className="flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white">Twitter</span>
+            <FaTwitter
+              size={30}
+              className="text-[#7abe41]"
+            />
+          </Link>
 
-                    <Link href='https://github.com/itsKirk' target='_blank' className='flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer'>
-                        <span className='font-bold text-white'>Github</span>
-                        <FaGithub size={30} className='text-[#7abe41]' />
-                    </Link>
-                    <Switch to='contact' smooth={true} className='flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer'>
-                        <span className='font-bold text-white'>E Mail</span>
-                        <HiOutlineMail size={30} className='text-[#7abe41]' />
-                    </Switch>
-                    <Link href='Patrick&apos;s CV.pdf' download='Patrick&apos;s CV.pdf' className='flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer'>
-                        <span className='font-bold text-white'>Resume</span>
-                        <BsPersonLinesFill size={30} className='text-[#7abe41]' />
-                    </Link>
-                </ul>
-            </div>
-            <div className="absolute top-24 mx-5">
-                {children}
-            </div>
-        </>
-    )
+          <Link
+            href="https://github.com/itsKirk"
+            target="_blank"
+            className="flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white">Github</span>
+            <FaGithub
+              size={30}
+              className="text-[#7abe41]"
+            />
+          </Link>
+          <Link
+            href="https://www.buymeacoffee.com/itsKirk"
+            target="_blank"
+            className="flex justify-between items-center w-[120px] ml-[-85px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white text-xs">Buy Me Coffee</span>
+            <FaCoffee
+              size={30}
+              className="text-[#7abe41]"
+            />
+          </Link>
+          <Switch
+            to="contact"
+            smooth={true}
+            className="flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white">E Mail</span>
+            <HiOutlineMail
+              size={30}
+              className="text-[#7abe41]"
+            />
+          </Switch>
+          <Link
+            href="Patrick's CV.pdf"
+            download="Patrick's CV.pdf"
+            className="flex justify-between items-center w-[100px] ml-[-65px] hover:ml-[0px] duration-500 cursor-pointer">
+            <span className="font-bold text-white">Resume</span>
+            <BsPersonLinesFill
+              size={30}
+              className="text-[#7abe41]"
+            />
+          </Link>
+        </ul>
+      </div>
+      <div className="absolute top-24 mx-5">{children}</div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
